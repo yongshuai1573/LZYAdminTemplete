b1e2051b421de661fd8864f909552d37699f1b6b
sch <156562384@qq.com>
2019-01-23 10:28:06 +0800

    modify

---
+++ js/common.js
@@ -0,0 +1,27 @@
+tableCheck = {
+  init: function() {
+    $(".layui-form-checkbox").click(function(event) {
+      if($(this).hasClass('layui-form-checked')) {
+        $(this).removeClass('layui-form-checked');
+        if($(this).hasClass('header')) {
+          $(".layui-form-checkbox").removeClass('layui-form-checked');
+        }
+      } else {
+        $(this).addClass('layui-form-checked');
+        if($(this).hasClass('header')) {
+          $(".layui-form-checkbox").addClass('layui-form-checked');
+        }
+      }
+    });
+  },
+  getData: function() {
+    var obj = $(".layui-form-checked").not('.header');
+    var arr = [];
+    obj.each(function(index, el) {
+      arr.push(obj.eq(index).attr('data-id'));
+    });
+    return arr;
+  }
+}
+//开启表格多选
+tableCheck.init();
